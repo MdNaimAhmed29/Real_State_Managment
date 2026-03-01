6ce9f18fbac4d25ebb7325612987b84f015f1c37
Naim Ahmed <naimahmed.vinacts@gmail.com>
2026-03-01 10:52:11 +0600

    login code update

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class LoginRequest extends FormRequest
@@ -42,15 +43,20 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        $user = User::where('email',)
+        $user = User::where('email',$this->login)
+                    ->orWhere('name',$this->login)
+                    ->orWhere('phone',$this->login)
+                    ->first();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if ( !$user || !Hash::check($this->password,$user->password) ) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
+                'login' => trans('auth.failed'),
             ]);
         }
+
+        Auth::login($user, $this->boolean('remember'));
 
         RateLimiter::clear($this->throttleKey());
     }
