09f75676f5ad90a3c25c551d54aa69a463c2c00c
Naim Ahmed <naimahmed.vinacts@gmail.com>
2026-03-01 19:53:28 +0600

    submit

--- routes/web.php
+++ routes/web.php
@@ -30,17 +30,20 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
-Route::middleware(['auth','role:admin'])->group(function(){
+Route::middleware(['auth', 'role:admin'])->group(function () {
 
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 
 });
 
 
-Route::middleware(['auth','role:agent'])->group(function(){
+Route::middleware(['auth', 'role:agent'])->group(function () {
 
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 
-}); 
+});
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
