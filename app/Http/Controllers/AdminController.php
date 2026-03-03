fb36c3e426cf86f9faae51254f23a21cb166c9ea
Naim Ahmed <naimahmed.vinacts@gmail.com>
2026-03-03 14:05:17 +0600

    password change functionality

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class AdminController extends Controller
@@ -53,7 +54,7 @@ class AdminController extends Controller
         $data->address = $request->address;
 
         if ($request->file('photo')) {
-            
+
             $file = $request->file('photo');
             @unlink(public_path('upload/admin_images/'.$data->photo));
             $filename = date('YmdHi').$file->getClientOriginalName();
@@ -73,6 +74,64 @@ class AdminController extends Controller
 
         return redirect()->back()->with($notification);
 
-    }
+    }//End Method
+
+
+    public function AdminChangePassword()
+    {
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+
+        return view('admin.admin_change_password',compact('profileData'));
+
+    }//End Method
+
+
+    public function AdminUpdatePassword(Request $request)
+    {
+
+        $request->validate([
+
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+
+        ]);
+
+
+        if (!Hash::check($request->old_password, auth::user()->password)) 
+
+        {
+
+            $notification = array(
+
+                'message' => 'Old Password Does not Matched',
+                'alert-type' => 'error'
+
+            );
+
+            return back()->with($notification);
+
+        }
+
+
+        user::whereId(auth()->user()->id)->update([
+
+            'password' => Hash::make($request->new_password)
+
+        ]);
+
+
+        $notification = array(
+
+            'message' => 'Password Changed Successfully',
+            'alert-type' => 'success'
+
+        );
+
+        return back()->with($notification);
+
+    }//End Method
+
 
 }
