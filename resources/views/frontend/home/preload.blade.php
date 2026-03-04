0280c5f317e388913a390eb80a97159a123d3fcf
Naim Ahmed <naimahmed.vinacts@gmail.com>
2026-03-04 12:46:43 +0600

    complete register and login form

---
+++ resources/views/frontend/home/preload.blade.php
@@ -0,0 +1,30 @@
+<div class="loader-wrap">
+    <div class="preloader">
+        <div class="preloader-close"><i class="far fa-times"></i></div>
+        <div id="handle-preloader" class="handle-preloader">
+            <div class="animation-preloader">
+                <div class="spinner"></div>
+                <div class="txt-loading">
+                   <span data-text-preloader="R" class="letters-loading">
+                       R
+                   </span>
+                   <span data-text-preloader="S" class="letters-loading">
+                       S
+                   </span>
+                   <span data-text-preloader="M" class="letters-loading">
+                       M
+                   </span>
+                   <span data-text-preloader="S" class="letters-loading">
+                       S
+                   </span>
+                   <span data-text-preloader="B" class="letters-loading">
+                       B
+                   </span>
+                   <span data-text-preloader="D" class="letters-loading">
+                       D
+                   </span>
+                </div>
+            </div>  
+        </div>
+    </div>
+</div>
